cb4f3fcf2174c01f52544ba09b661fb9b3aa18f7
DMNE <66816413+brandonjanssen@users.noreply.github.com>
2023-04-19 09:49:27 +0800

    Add files via upload

---
+++ LEI.py
@@ -0,0 +1,205 @@
+OUTCOME: Dead Air
+PH:     +12403689307
+REASON: dead air
+
+Outcome:Already has Atty
+Ph:  +13049522565
+Reason: cx already signed up
+
+
+OUTCOME: Did Not Apply
+PH:     +12483192361 
+REASON: cx didn't have a diagnosis on the list
+
+Outcome:DID NOT LIVE ,WORK,SERVE
+Ph:   +17022804091 
+Reason:DID NOT LIVE ,WORK,SERVE AT CAMP LEJEUNE
+
+OUTCOME: SUCCESS
+PH:     +15406327063
+REASON: CX CONNECTED TO Diabetic
+
+OUTCOME: SUCCESS
+PH:             +16028628351
+REASON: CX and LA connected
+
+Outcome:DQ BAD INS
+Ph:     +12703771796
+Reason: no out of network benifits medicade
+
+OUTCOME: Callback Scheduled
+PH:       +12152893171 
+REASON: cx is interested cb set
+
+Outcome: Dropped CB now
+Phone:    +12394402482 
+Reason: CB now
+
+OUTCOME: HUNG UP
+PH:   +13139127353
+REASON: CX hu  |cb set
+
+Outcome: LV
+Phone:    +16787916251
+Reason: routed to VM
+
+Outcome: AM
+Phone:      +18322467276
+Reason: Answering Machine
+
+Outcome: AM
+Phone:      +12542833006 
+Reason: vm full
+
+Outcome: AM
+Phone:   +15743526321
+Reason: vm not set up
+
+Outcome:NI
+Ph:       +12522959246
+Reason:CX NI in  insurance
+
+Outcome:NI/upset
+Ph:   +16083861596
+Reason:CX NI not INTERESTED and slightly upset
+
+Outcome:TRANSFER FAILED / HU
+Ph:   +18329748881 
+Reason:CX HU went silent
+
+Outcome:TRANSFER FAILED / set callback
+Ph:      +18438602827 
+Reason:CX no LA available
+
+Outcome:ALREADY ENROLLED
+Ph:      +16788153958 
+Reason:CX all ready has insurance
+
+# ALL READY HAS GOVERMENT BENIFTS ###########
+# ALL READY HAS GOVERMENT BENIFTS ###########
+Outcome:DQ GOVT INSURANCE
+Ph:      +19017349104 
+Reason:GOVT INSURANCE foodstamps
+
+# CANT AFFORD    ##########################
+# CANT AFFORD    ##########################
+Outcome:DQ CAN'T AFFORD
+Ph:  +13364378366 
+Reason:BELOW 13K
+
+# DISQUALFIFED HAND OFF FRONTIER #############
+# DISQUALFIFED HAND OFF FRONTIER #############
+Outcome:DQ HANDOFF
+Ph:     +16019914657
+Reason:FRONTER TALKING / ADVISED / CB      OR
+AGENTS TAKING CALLS IN THE BACKGROUND
+
+# PANKER  ################################
+# PANKER  ################################
+Outcome:PRANK CALLER
+Ph:   +19013259918
+Reason:JUST PLAYING AROUND
+
+# ADD To DO NOT CALL LIST ####################
+# ADD To DO NOT CALL LIST ####################
+Outcome:DNC
+Ph:    +16019878978
+Reason:PROFANITY  OR CX REQ
+
+# ADD To DO NOT CALL LIST ####################
+# ADD To DO NOT CALL LIST ####################
+Outcome:DNC
+Ph:   +18033472341 
+Reason:cx no more calls please
+
+# COACHED CALL  ###########################
+# COACHED CALL  ###########################
+Outcome:DQ COACHED CALL
+Ph:  +18709401243 
+Reason: BEING COACHED
+CALLED AGAIN AND GAVE DIFFERENT INFO TO QUALIFY
+
+# WOULD NOT AGREE TO DISCLAMER  ##############
+# WOULD NOT AGREE TO DISCLAMER  ##############
+Outcome:TCPA FAIL
+Ph:   +15203325495
+Reason:NO TO THE DISCLAIMER
+DID NOT SAY A CLEAR YES TO THE DISCLAIMER
+
+# INCENTIVIZED  #############################
+# INCENTIVIZED  #############################
+Outcome:DQ INCENTIVIZED
+Ph: +19187152757
+Reason: WAS PROMISED SOMETHING (MONEY OR GIFT CARDS)
+
+# DQ BAD STATE  #############################
+# DQ BAD STATE  #############################
+OUTCOME: dq bad state
+PH:    +18333704442
+REASON: no avail LA /CT 4x
+
+# BAD CONNECTION ###########################
+# BAD CONNECTION ###########################
+OUTCOME: dq bad connection
+PH:   +19729401666 
+REASON: lots of stati
+
+#   NO ENGLISH     #############################
+#   NO ENGLISH     #############################
+OUTCOME:Does not speak English
+PH:  +14053941419
+REASON:speak spanish
+
+
+# NUMBER NOT IN SERVICE #######################
+OUTCOME: number not in service
+PH#:13347967561
+REASON:
+
+# ADD TO DNC ################################
+OUTCOME: ADD TO DNC
+PH:    +12394402482 
+REASON: Does not want calls
+
+# DID NOT APPLY  ##############################
+OUTCOME: did not apply
+Ph:   +13218328788
+Reason: or wrong number
+
+# WRONG NUMBER #############################
+# WRONG NUMBER #############################
+OUTCOME: Wrong number
+PH:      +13526446862 
+REASON: Fronters Number
+
+outcome: tcpa fail
+ph: +18034183127
+reason: no clear yes
+
+outcome: dq age
+ph:     +12563086615 
+reason: born in the yr. 1952
+
+outcome: dq bad ins
+ph:     +17542046149 
+reason: medicaid
+
+outcome: dq no cell phone
+ph:     +16012328035
+reason: does not want to get a cell phone
+
+outcome: dq bad diagnois
+ph:    +18043197775 
+reason: no diabetes or  hypertension
+
+DQ - Not using Monitor or BP cuff
+ph:   +16176902200 
+reason: not monitoring their self
+
+outcome: dq mental issue
+ph:   +13854988648
+reason: incoherent
+
+
++17736101735
+ +17087332868
---
+++ bin/README.md
@@ -0,0 +1,5 @@
+# scripts for Spectrwm
+- scratchnn.sh: is a scratchpad NNN file manager and is to be added to .local/bin
+- scmoc.sh: is a scratchpad MOC terminal music player and is to be added to .local/bin
+- launch-fzf.sh is a rofi or dmenu replacement is meant to be used for NNN  put in .local/bin  
+    - launch in you confi kitty -e .local/bin/launch-fzf.sh
